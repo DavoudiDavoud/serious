dc796b0a47835ade69cdf099796c52587460e567
DavoudiDavoud <davoudi.davoud@yahoo.com>
2016-04-13 07:06:58 +0100

    Update adcreader.h

--- adcreader.h
+++ adcreader.h
@@ -1,6 +1,17 @@
 #ifndef ADCREADER
 #define ADCREADER
+//#include <QDebug>
+#include <stdint.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/types.h>
+#include <linux/spi/spidev.h>
 
+#include "gz_clk.h"
+#include "gpio-sysfs.h"
 #include <QThread>
  
 class ADCreader : public QThread
